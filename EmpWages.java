cb84195235103c5706c4976b321380f6e7e70f18
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-03-01 22:30:38 +0530

    Added : code for to calculate daily wage for 20 days

--- EmpWages.java
+++ EmpWages.java
@@ -4,35 +4,44 @@ public class EmpWages {
   public static final int IS_FULL_TIME = 1;
   public static final int IS_PART_TIME = 2;
   public static final int WAGE_PER_HOUR = 20;
+  public static int WORKING_DAY_PER_MONTH = 20;
     int empHrs = 0;
+    int wageForMonth = 0;
+    int dailyWage = 0;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee  Wage Computation :");
         EmpWages empWages = new EmpWages();
         empWages.checkDailyEmpWage();
-        empWages.dailyWage();
+        empWages.monthlyWage();
     }
 
     public void checkDailyEmpWage() {
+        for (int days=0; days< WORKING_DAY_PER_MONTH; days++) {
 
-        double empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch ((int) empCheck) {
-            case IS_PART_TIME:
-                empHrs = 4;
-                System.out.println("Employee Present Half time :");
-                break;
-            case IS_FULL_TIME:
-                empHrs = 8;
-                System.out.println("Employee Present Full time :");
-                break;
-            default:
-                empHrs = 0;
-                System.out.println("Employee Absent :");
+            double empCheck = Math.floor(Math.random() * 10) % 3;
+            switch ((int) empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    System.out.println("Employee is Present Full time : ");
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    System.out.println("Employee is Present Half time : ");
+                    break;
+                default:
+                    empHrs = 0;
+                    System.out.println("Employee is Absent : ");
+                    break;
+            }
+            dailyWage = (empHrs * WAGE_PER_HOUR) ;
+            System.out.println("Daily wage for employee : "+dailyWage+ "$");
+            wageForMonth  = dailyWage + wageForMonth;
         }
 
     }
 
-    public void dailyWage() {
-        int dailyWage = (empHrs * WAGE_PER_HOUR);
-        System.out.println("Daily wage for employee : "+dailyWage+ "$");
+    public void monthlyWage() {
+        System.out.println("Wage for month "+wageForMonth);
     }
 }
