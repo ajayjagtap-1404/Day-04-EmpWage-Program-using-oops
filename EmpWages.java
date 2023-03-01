85335a0345b235d5f5a32f2d605c467a84e3636d
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-03-01 21:13:45 +0530

    Added : code for to check part time emp

--- EmpWages.java
+++ EmpWages.java
@@ -2,6 +2,7 @@ package com.assigenement.day_3.employee_wage_using_object;
 
 public class EmpWages {
     int IS_FULL_TIME = 1;
+    int IS_PART_TIME = 2;
     int WAGE_PER_HOUR = 20;
     int emHrs = 0;
     public static void main(String[] args) {
@@ -13,20 +14,26 @@ public class EmpWages {
 
     public void checkDailyEmpWage() {
 
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
 
         if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present full time : ");
+
+            System.out.println("Employee is Present Full time : ");
             emHrs = 8;
+
+        } else if (empCheck == IS_PART_TIME){
+            System.out.println("Employee is Present Half time : ");
+            emHrs = 4;
+
         } else {
             System.out.println("Employee is Absent : ");
-            emHrs=0;
+            emHrs = 0;
         }
+
     }
+
     public void dailyWage() {
         int dailyWage = (emHrs * WAGE_PER_HOUR);
         System.out.println("Daily wage for employee : "+dailyWage+ "$");
-
     }
-
 }
