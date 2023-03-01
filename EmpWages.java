4f83d529dc81251f6aefd3803140c806e470fa8c
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-03-01 21:25:19 +0530

    Refactring the code using switch case

--- EmpWages.java
+++ EmpWages.java
@@ -1,10 +1,10 @@
 package com.assigenement.day_3.employee_wage_using_object;
 
 public class EmpWages {
-    int IS_FULL_TIME = 1;
-    int IS_PART_TIME = 2;
-    int WAGE_PER_HOUR = 20;
-    int emHrs = 0;
+  public static final int IS_FULL_TIME = 1;
+  public static final int IS_PART_TIME = 2;
+  public static final int WAGE_PER_HOUR = 20;
+    int empHrs = 0;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee  Wage Computation :");
         EmpWages empWages = new EmpWages();
@@ -14,26 +14,25 @@ public class EmpWages {
 
     public void checkDailyEmpWage() {
 
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-
-        if (empCheck == IS_FULL_TIME) {
-
-            System.out.println("Employee is Present Full time : ");
-            emHrs = 8;
-
-        } else if (empCheck == IS_PART_TIME){
-            System.out.println("Employee is Present Half time : ");
-            emHrs = 4;
-
-        } else {
-            System.out.println("Employee is Absent : ");
-            emHrs = 0;
+        double empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch ((int) empCheck) {
+            case IS_PART_TIME:
+                empHrs = 4;
+                System.out.println("Employee Present Half time :");
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                System.out.println("Employee Present Full time :");
+                break;
+            default:
+                empHrs = 0;
+                System.out.println("Employee Absent :");
         }
 
     }
 
     public void dailyWage() {
-        int dailyWage = (emHrs * WAGE_PER_HOUR);
+        int dailyWage = (empHrs * WAGE_PER_HOUR);
         System.out.println("Daily wage for employee : "+dailyWage+ "$");
     }
 }
