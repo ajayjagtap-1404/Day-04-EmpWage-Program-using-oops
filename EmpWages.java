5556af956a40a909247434c8df6af0ff2b1f14cd
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-03-01 20:41:38 +0530

    Initiate : Emp Wage program

---
+++ EmpWages.java
@@ -0,0 +1,13 @@
+package com.assigenement.day_3.employee_wage_using_object;
+
+public class EmpWages {
+    public static void main(String[] args) {
+        EmpWages ms = new EmpWages();
+        ms.showingMsg();
+    }
+
+    public void showingMsg() {
+        System.out.println("Welcome to Employee  Wage Computation :");
+
+    }
+}
