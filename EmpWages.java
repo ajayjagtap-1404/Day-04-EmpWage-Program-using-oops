76e5ab4d289e392cd74078f2fae5cc30a4f209ac
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-03-03 13:30:04 +0530

    updating problem with the class level variables and methods

--- EmpWages.java
+++ EmpWages.java
@@ -1,56 +1,69 @@
 package com.assigenement.day_3.employee_wage_using_object;
 
+import java.util.Random;
+
 public class EmpWages {
-  public static final int IS_FULL_TIME = 1;
-  public static final int IS_PART_TIME = 2;
-  public static final int WAGE_PER_HOUR = 20;
-  public static int MAX_DAY_PER_MONTH = 20;
-
-  public static int MAX_HRS_IN_A_MONTH = 100;
-    int wageForMonth = 0;
-
+    static final int IS_Full_TIME = 1;
+    static final int IS_PART_TIME = 2;
+    final static int WAGE_PER_HOUR = 20;
+    final static int MAX_WORKING_DAYS = 20;
+    final static int MAX_WORKING_HOURS = 60;
+    static int empHrs;
+    static int wageForMonth;
+    static int total_Working_Hours;
+    static int total_Working_Days;
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee  Wage Computation :");
-        EmpWages empWages = new EmpWages();
-        System.out.println("Wage for Month : "+empWages.totalWage(empWages.wageForMonth));
+        System.out.println("Welcome to Emp Wage Program :");
+       EmpWages empWages = new EmpWages();
+        empWages.totalWage();
+
 
     }
 
-    public int totalWage(int wageForMonth) {
-        int numberOfWorkingDays = 0;
-        int totalWorkingHrs = 0;
-        int empHrs = 0;
-        int dailyWage = 0;
-
-
-        while (numberOfWorkingDays < MAX_DAY_PER_MONTH && totalWorkingHrs < MAX_HRS_IN_A_MONTH) {
-            numberOfWorkingDays++;
-
-            double empCheck = Math.floor(Math.random() * 10) % 3;
-            switch ((int) empCheck) {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    System.out.println("Employee is Present Full time : ");
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    System.out.println("Employee is Present Half time : ");
-                    break;
-                default:
-                    empHrs = 0;
-                    System.out.println("Employee is Absent : ");
-                    break;
+    public static void totalWage() {
+        for (int i = 1; i <= MAX_WORKING_DAYS; i++) {
+            if (total_Working_Days < MAX_WORKING_DAYS && total_Working_Hours < MAX_WORKING_HOURS ) {
+                EmpWages.empCheck();
+                //
+                int dailyWage = empHrs * WAGE_PER_HOUR;
+                System.out.println("Daily Wage = " +dailyWage);
+                total_Working_Days++;
+                System.out.println("Total working Hours : " +total_Working_Hours+ " for day : " + i  );
+                wageForMonth = wageForMonth + dailyWage;
+                System.out.println();
             }
-
-            dailyWage = (empHrs * WAGE_PER_HOUR);
-            totalWorkingHrs = (empHrs + totalWorkingHrs);
-            System.out.println("Daily wage for employee : " + dailyWage + "$");
-            wageForMonth = dailyWage + wageForMonth;
         }
-        return wageForMonth;
+        System.out.println("Total Wage for the month is: " + wageForMonth + " Total working days are: "
+                + total_Working_Days + " Total working hours are: " + total_Working_Hours);
+    }
 
+    public static void empCheck() {
+        Random random = new Random();
+        int isPresent = random.nextInt(3);
 
-
+        switch (isPresent) {
+            case IS_Full_TIME : {
+                if (total_Working_Hours + 8 <= MAX_WORKING_HOURS) {
+                    System.out.println("Employee is present Full time");
+                    empHrs = 8;
+                    // calculating total working hours
+                    total_Working_Hours = total_Working_Hours + empHrs;
+                }
+            }
+            break;
+            case IS_PART_TIME: {
+                if (total_Working_Hours + 8 <= MAX_WORKING_HOURS) {
+                    System.out.println("Employee is present Part time");
+                    empHrs = 4;
+                    total_Working_Hours = total_Working_Hours + empHrs;
+                }
+            }
+            break;
+            default:
+                System.out.println("Employee is Absent");
+                empHrs = 0;
+                break;
+        }
     }
 
 
